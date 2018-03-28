7821a462e7a88c929b1e8616e96b341d990a5a0c
Artem Maslovskiy <em.arcamenel@gmail.com>
2018-03-28 10:15:26 +0300

    add more interpreter tests

--- paintfuck/tests/test_interpreter.py
+++ paintfuck/tests/test_interpreter.py
@@ -9,7 +9,7 @@ class InterpreterTestCase(unittest.TestCase):
         actual_output = interpreter.run_program(program, iterations)
         self.assertEqual(expected_output, actual_output)
 
-    def test_interpreting_simple_command(self):
+    def test_interpreting_simple_program(self):
         self._compare_program_with_output(
             program='*s*',
             iterations=3,
@@ -22,7 +22,7 @@ class InterpreterTestCase(unittest.TestCase):
             ]
         )
 
-    def test_interpreting_simple_command_with_redundant_iterations(self):
+    def test_interpreting_simple_program_with_excess_iterations(self):
         self._compare_program_with_output(
             program='*s*',
             iterations=100,
@@ -31,6 +31,56 @@ class InterpreterTestCase(unittest.TestCase):
             expected_output=[
                 [1, 0, 0],
                 [1, 0, 0],
+                [0, 0, 0]
+            ]
+        )
+
+    def test_interpreting_simple_program_with_insufficient_iterations(self):
+        self._compare_program_with_output(
+            program='*s*',
+            iterations=2,
+            width=3,
+            height=3,
+            expected_output=[
+                [1, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0]
+            ]
+        )
+
+    def test_interpreting_simple_program_with_non_command_chars(self):
+        self._compare_program_with_output(
+            program='X*Ys*Z',
+            iterations=6,
+            width=3,
+            height=3,
+            expected_output=[
+                [1, 0, 0],
+                [1, 0, 0],
+                [0, 0, 0]
+            ]
+        )
+
+    def test_interpreting_simple_loops(self):
+        self._compare_program_with_output(
+            program='*[s]*',
+            iterations=10,
+            width=3,
+            height=3,
+            expected_output=[
+                [1, 0, 0],
+                [1, 0, 0],
+                [0, 0, 0]
+            ]
+        )
+        self._compare_program_with_output(
+            program='[s]e*',
+            iterations=10,
+            width=3,
+            height=3,
+            expected_output=[
+                [0, 1, 0],
+                [0, 0, 0],
                 [0, 0, 0]
             ]
         )
