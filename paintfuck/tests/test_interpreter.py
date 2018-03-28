9f20eff15a2e99b05395b87e8e4fad534a9a3120
Artem Maslovskiy <em.arcamenel@gmail.com>
2018-03-28 09:51:38 +0300

    implement interpreter logic

---
+++ paintfuck/tests/test_interpreter.py
@@ -0,0 +1,23 @@
+import unittest
+
+from paintfuck.interpreter import Interpreter
+
+
+class InterpreterTestCase(unittest.TestCase):
+    def _compare_program_with_output(self, program, iterations, width, height, expected_output):
+        interpreter = Interpreter(width, height)
+        actual_output = interpreter.run_program(program, iterations)
+        self.assertEqual(expected_output, actual_output)
+
+    def test_interpreting_simple_command(self):
+        self._compare_program_with_output(
+            program='*s*',
+            iterations=3,
+            width=3,
+            height=3,
+            expected_output=[
+                [1, 0, 0],
+                [1, 0, 0],
+                [0, 0, 0]
+            ]
+        )
