91b2d0b32546c8fd23e8a743945704b58434cec3
Artem Maslovskiy <artem.maslovskiy@3d4medical.com>
2018-03-28 15:01:25 +0300

    add bitfield to str converter

--- paintfuck/tests/test_utils.py
+++ paintfuck/tests/test_utils.py
@@ -1,6 +1,6 @@
 import unittest
 
-from paintfuck.utils import cycle_capped
+from paintfuck.utils import cycle_capped, bitfield_to_str
 
 
 class UtilitiesTestCase(unittest.TestCase):
@@ -25,3 +25,25 @@ class UtilitiesTestCase(unittest.TestCase):
         self.assertEqual(0, cycle_capped(10, 0, 10))
         self.assertEqual(99, cycle_capped(99, 0, 100))
         self.assertEqual(0, cycle_capped(100, 0, 100))
+
+    def test_bitfield_to_str(self):
+        self.assertEqual('00\r\n00', bitfield_to_str(
+            [
+                [0, 0],
+                [0, 0],
+            ]
+        ))
+        self.assertEqual('010\r\n101\r\n111', bitfield_to_str(
+            [
+                [0, 1, 0],
+                [1, 0, 1],
+                [1, 1, 1],
+            ]
+        ))
+        self.assertEqual('111\r\n000\r\n001', bitfield_to_str(
+            [
+                [True, True, True],
+                [False, False, False],
+                [False, False, True],
+            ]
+        ))
--- paintfuck/utils.py
+++ paintfuck/utils.py
@@ -4,3 +4,10 @@ def cycle_capped(value, low, top):
     if value >= top:
         return low
     return value
+
+
+def bitfield_to_str(field):
+    return '\r\n'.join(
+        ''.join(str(int(bit)) for bit in row)
+        for row in field
+    )
