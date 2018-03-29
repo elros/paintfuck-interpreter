4441f080c4b7e0dc58690a326aeba6ad36206ad3
Artem Maslovskiy <em.arcamenel@gmail.com>
2018-03-29 09:46:58 +0300

    add more complex tests

--- paintfuck/tests/test_main.py
+++ paintfuck/tests/test_main.py
@@ -19,3 +19,62 @@ class MainModuleTestCase(unittest.TestCase):
             width=2,
             height=2
         ))
+
+    def test_main_module_with_complex_program(self):
+        code = '*e*e*e*es*es*ws*ws*w*w*w*n*n*n*ssss*s*s*s*'
+        width = 6
+        height = 9
+
+        def partial_run(iterations):
+            return interpreter(code, iterations, width, height)
+
+        test_cases = [
+            (0, '\r\n'.join(['000000',
+                             '000000',
+                             '000000',
+                             '000000',
+                             '000000',
+                             '000000',
+                             '000000',
+                             '000000',
+                             '000000'])),
+            (7, '\r\n'.join(['111100',
+                             '000000',
+                             '000000',
+                             '000000',
+                             '000000',
+                             '000000',
+                             '000000',
+                             '000000',
+                             '000000'])),
+            (19, '\r\n'.join(['111100',
+                              '000010',
+                              '000001',
+                              '000010',
+                              '000100',
+                              '000000',
+                              '000000',
+                              '000000',
+                              '000000'])),
+            (42, '\r\n'.join(['111100',
+                              '100010',
+                              '100001',
+                              '100010',
+                              '111100',
+                              '100000',
+                              '100000',
+                              '100000',
+                              '100000'])),
+            (100, '\r\n'.join(['111100',
+                               '100010',
+                               '100001',
+                               '100010',
+                               '111100',
+                               '100000',
+                               '100000',
+                               '100000',
+                               '100000'])),
+        ]
+
+        for iterations, result in test_cases:
+            self.assertEqual(result, partial_run(iterations))
